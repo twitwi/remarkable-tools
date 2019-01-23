144d6259f4b53ab85dcb22bd016e3b1af15566b0
Rémi Emonet <remi.emonet@reverse--com.heeere>
2019-01-23 02:14:16 +0100

    Allow sending patches to lower SSH transfer
    can be improved as undo etc does not always work

--- liveview/on-remarkable-watch.sh
+++ liveview/on-remarkable-watch.sh
@@ -14,14 +14,41 @@ log $@
 ps | \grep 'on\-remarkable' | awk '{ print $1 }' | \grep -v "^$$\$" | awk '{print $1}' | xargs kill
 
 last=$(mktemp)
+now=$(mktemp)
+err=$(mktemp)
 lastsize=0
 log $last
 
-sendit() {
-        echo START
-        wc -c < "$1"
-        cat "$1"
-        echo END
+sendfull() {
+  echo FULL
+  wc -c < "$1"
+  cat "$1"
+  echo END
+}
+sendpatch() {
+  echo PATCH
+  echo "$diff"
+  if \grep -q -e "$3" "$2" ; then
+    echo APPEND
+    local s
+    s=$(( $6 - $5 ))
+    echo $s
+    tail -c -$s "$4"
+  fi
+  if \grep -q -e "$4" "$2" ; then
+    echo TRUNC
+    echo $6
+  fi
+  echo END
+}
+sendauto() { # "$diff" "$err_f" "$last_f" "$now_f" "$lastsize" "$size"
+  if test "$1" = "" -o "$5" = 0 ; then
+    sendfull "$4"
+  elif test $(echo "$1" | wc -l) = 100 ; then
+    sendfull "$4"
+  else
+    sendpatch "$@"
+  fi
 }
 
 if test "$1" = "//" ; then
@@ -41,15 +68,19 @@ fi
 
 log $1
 log $p
-sendit "$p"
+#sendfull "$p"
 
 while true ; do
-    if test \! "$p" -ot "$last" ; then
-        size=$(cat "$p" | wc -c)
-        if test "$size" -gt "$lastsize" -o "$size" -lt 100 ; then
-            lastsize=$size
-            touch "$last"
-            sendit "$p"
+    if test \! "$p" -ot "$last" -o "$lastsize" = 0 ; then
+        size=$(wc -c < "$p")
+        if test "$size" -ne "$lastsize" ; then
+          cp "$p" "$now"
+          size=$(wc -c < "$now")
+          diff=$(cmp -l "$last" "$now" 2> "$err" | head -n 100)
+          sendauto "$diff" "$err" "$last" "$now" "$lastsize" "$size"
+
+          cp "$now" "$last"
+          lastsize=$size
         fi
     else
         # show liveliness + trigger auto-kill on disconnection
--- liveview/to-view.py
+++ liveview/to-view.py
@@ -19,6 +19,21 @@ def read_line(f=sys.stdin):
     return res.decode('utf-8')
 
 all = []
+async def rebuild_svg(file_rm, file_svg, notify=True):
+    try:
+        rm2svg(["-c", "-i", file_rm, "-o", file_svg])
+    except:
+        print("ERROR WITH RUNNING RM->SVG")
+
+    if notify:
+        a = all.copy()
+        all[:] = []
+        for ws in a:
+            try:
+                await ws.send("Salut "+str(len(all)))
+                all.append(ws)
+            except:
+                pass
 
 async def log(websocket, path):
     print("START LOG", websocket)
@@ -43,8 +58,10 @@ async def parse_input(ws_sv, file_rm, file_svg):
         except:
             print("ERR", line)
             continue
+
         print("LINE:", line, len(all))
-        if line == "START":
+
+        if line == "FULL":
             count = await asyncio.get_event_loop().run_in_executor(None, read_line)
             count = int(count)
             print("READING", count)
@@ -52,20 +69,42 @@ async def parse_input(ws_sv, file_rm, file_svg):
             print("READ", len(data))
             with open(file_rm, 'wb') as f:
                 f.write(data)
+            await asyncio.get_event_loop().run_in_executor(None, read_line) # read END
+            await rebuild_svg(file_rm, file_svg)
 
-            try:
-                rm2svg(["-c", "-i", file_rm, "-o", file_svg])
-            except:
-                print("ERROR WITH RUNNING RM->SVG")
+        elif line == "PATCH":
+            print("LINE", line)
+            patch = {}
+            more = b''
+            while True:
+                line = await asyncio.get_event_loop().run_in_executor(None, read_line)
+                if line == "APPEND":
+                    count = await asyncio.get_event_loop().run_in_executor(None, read_line)
+                    count = int(count)
+                    print("READING", count)
+                    more = await asyncio.get_event_loop().run_in_executor(None, read_chunk_from_stdin(count))
+                elif line == "TRUNC":
+                    size = await asyncio.get_event_loop().run_in_executor(None, read_line)
+                    size = int(size)
+                    print("TRUNC to", size)
+                    with open(file_rm, 'wb') as f:
+                        f.seek(size, 0)
+                        f.write(bytes([]))
+                elif line == "END":
+                    break
+                else:
+                    els = line.replace('  ', ' ').replace('  ', ' ').split(" ")
+                    if len(els) == 3:
+                        patch[int(els[0])] = int(els[2], 8)
 
-            a = all.copy()
-            all[:] = []
-            for ws in a:
-                try:
-                    await ws.send("Salut "+str(len(all)))
-                    all.append(ws)
-                except:
-                    pass
+            with open(file_rm, 'rb') as f:
+                content = bytearray(f.read())
+            for k,v in patch.items():
+                content[k-1] = v
+            content += more
+            with open(file_rm, 'wb') as f:
+                f.write(content)
+            await rebuild_svg(file_rm, file_svg)
 
         print(len(all))
         #await ws_server.send("SALUT!\n")
